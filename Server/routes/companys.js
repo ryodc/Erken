a8536615415afdbc859f3ce1550f7448fba1d5ec
DzemalMRVC <70542783+DzemalMRVC@users.noreply.github.com>
2022-05-17 13:13:33 +0200

    Backend
    Backend

---
+++ Server/routes/companys.js
@@ -0,0 +1,36 @@
+import express from "express";
+import {
+  updateCompany,
+  deleteCompany,
+  getCompany,
+  getCompanys,
+} from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyCompany } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+// router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+//   res.send("hello user, you are logged in")
+// })
+
+// router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
+//   res.send("hello user, you are logged in and you can delete your account")
+// })
+
+// router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
+//   res.send("hello admin, you are logged in and you can delete all accounts")
+// })
+
+//UPDATE
+router.put("/:id", verifyCompany, updateCompany);
+
+//DELETE
+router.delete("/:id", verifyCompany, deleteCompany);
+
+//GET
+router.get("/:id", verifyCompany, getCompany);
+
+//GET ALL
+router.get("/", verifyAdmin, getCompanys);
+
+export default router;
