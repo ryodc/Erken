12dc61e713744882efd2b6ddcbb0cb3e759e1e9a
JoelvanSeventer <80532479+JoelvanSeventer@users.noreply.github.com>
2022-05-17 16:50:51 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import Landingpage from "./Components/Default/landingpage.js";
 // import React from "react";
+/*
+import Feed from "./Feed/feed";
+*/
+import Profile from "./Components/Profile/profile.js";
+
 
 const App = () => {
   return <Landingpage />;
