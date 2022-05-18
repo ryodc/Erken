5ad6fcb3299ee9a1d0d767e48f6c2b4e76ba8078
DanIsVis <danielvisser.nl@gmail.com>
2022-05-18 16:05:16 +0200

    heel goed
    POGGERS

--- client/src/Components/Vacature/vacature.js
+++ client/src/Components/Vacature/vacature.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./vacature.css";
+import Logo from "../Images/ErkenLogoScalable.png";
+/*
+import LockIcon from "@mui/icons-material/Lock";
+*/
 
 const Vacature = () => {
-    return (
-        <div class="vacatureHead">
-        <h1>Vacature</h1>
+  return (
+    <div className="profileHead">
+      <div className="profile">
+        <div>
+            <input className="Searchbar" placeholder="Zoeken"></input>
         </div>
-    );
-}
-
+      </div>
+    </div>
+  );
+};
 export default Vacature;
--- client/src/components/Default/sidebardata.js
+++ client/src/components/Default/sidebardata.js
@@ -1,29 +1,31 @@
 import React from 'react'
-
-
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import PersonIcon from '@mui/icons-material/Person';
 
 export const SidebarData = [
   {
     title: "Feed",
-    // icon: <HomeIcon />,
+    icon: <HomeIcon />,
     path: "/",
     cName: 'nav-text'
   },
   {
-    title: "Vacatures",
-    // icon: <EventIcon />,
-    path: "/Agenda",
+    title: "Vacature",
+    icon: <SearchIcon />,
+    path: "/Vacature",
     cName: 'nav-text'
   },
   {
     title: "Chat",
-    // icon: <ChatBubbleIcon />,
+    icon: <ChatBubbleIcon />,
     path: "/Chat",
     cName: 'nav-text'
   },
   {
     title: "Profile",
-    // icon: <AccountCircleIcon />,
+    icon: <PersonIcon />,
     path: "/Profile",
     cName: 'nav-text'
   }
