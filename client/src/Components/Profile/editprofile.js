12cee9885ea7853665200040f9ab4c287f5a69aa
JoelvanSeventer <80532479+JoelvanSeventer@users.noreply.github.com>
2022-05-24 16:29:51 +0200

    profile + editprofile update

--- client/src/Components/Profile/editprofile.js
+++ client/src/Components/Profile/editprofile.js
@@ -1,62 +1,41 @@
 import React from "react";
-import "./profile.css";
+import "./editprofile.css";
 import Logo from "../Images/ErkenLogoScalable.png";
-import LockIcon from "@mui/icons-material/Lock";
+import LockOpenIcon from '@mui/icons-material/LockOpen';
 import SearchIcon from "@mui/icons-material/Search";
 
 import { useState } from "react";
 
-export const EditProfile = ({ showShowEvent, setShowEvent }) => {
+export const EditProfile = ({ showEditProfile, setEditProfile }) => {
 
-  const [showEditProfile, setEditProfile] = useState(false);
+  const [Edit, setEdit] = useState(false);
 
   const openEditProfile = () => {
     setEditProfile((prev) => !prev);
   };
 
   return (
-    <div className="profileHead">
-      <img className="logo" src={Logo} alt="Logo"></img>
-      <div className="profile">
-        <div>
-          <img
-            className="profileImage"
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="profile"
-          />
-
-
-          <EditProfile showEditProfile={showEditProfile} setEditProfile={setEditProfile} />
-          <button className="EditProfile" onClick={openEditProfile}><LockIcon/></button>
-
-
-              <div className="profileContainer">
-                <h2>Profiel</h2>
-                <div className="ProfileDataNamesContainer">
-                  <p>Naam</p>
-                  <p>Email</p>
-                  <p>Telefoonnummer</p>
-                  <p>Email verzorger</p>
-                  <p>Telefoonnummer verzorger</p>
-                  <p>Straat</p>
-                  <p>Huisnummer</p>
-                  <p>Woonplaats</p>
-                  <p>Postcode</p>
-                </div>
-                <div className="profileData">
-                  <input className="EditdataBoxes" placeholder="Hogeschool Rotterdam"></input>
-                  <input className="EditdataBoxes" placeholder="hr@outlook.com"></input>
-                  <input className="EditdataBoxes" placeholder="010 794 4000"></input>
-                  <input className="EditdataBoxes" placeholder="0000000@hr.nl"></input>
-                  <input className="EditdataBoxes" placeholder="010 794 4000"></input>
-                  <input className="EditdataBoxes" placeholder="Wijnhaven"></input>
-                  <input className="EditdataBoxes" placeholder="107"></input>
-                  <input className="EditdataBoxes" placeholder="Rotterdam"></input>
-                  <input className="EditdataBoxes" placeholder="3011 WN"></input>
-                </div>
-              </div>
+    <div> 
+      {showEditProfile ? (
+    
+      <div className="profileHead">
+        <div className="profile">
+            <div className="EditprofileData">
+              <input className="EditdataBoxes" placeholder="Hogeschool Rotterdam"></input>
+              <input className="EditdataBoxes" placeholder="hr@outlook.com"></input>
+              <input className="EditdataBoxes" placeholder="010 794 4000"></input>
+              <input className="EditdataBoxes" placeholder="0000000@hr.nl"></input>
+              <input className="EditdataBoxes" placeholder="010 794 4000"></input>
+              <input className="EditdataBoxes" placeholder="Wijnhaven"></input>
+              <input className="EditdataBoxes" placeholder="107"></input>
+              <input className="EditdataBoxes" placeholder="Rotterdam"></input>
+              <input className="EditdataBoxes" placeholder="3011 WN"></input>
+            </div>
         </div>
+        <button className="Backbutton" onClick={() => setEditProfile((prev) => !prev)}><LockOpenIcon/></button>
+        <button className="Savebutton" onClick={() => setEditProfile((prev) => !prev)}>Opslaan</button>
       </div>
+      ) : null }
     </div>
   );
 };
--- client/src/Components/Profile/profile.js
+++ client/src/Components/Profile/profile.js
@@ -5,65 +5,69 @@ import Logo from "../Images/ErkenLogoScalable.png";
 import LockIcon from "@mui/icons-material/Lock";
 import SearchIcon from "@mui/icons-material/Search";
 import Search from "@mui/icons-material/Search";
+import AttachmentIcon from '@mui/icons-material/Attachment';
 
 import { EditProfile } from "./editprofile.js";
 import { useState } from "react";
 
 
 function Profile () {
-  /*
+  
   const [showEditProfile, setEditProfile] = useState(false);
 
   const openEditProfile = () => {
     setEditProfile((prev) => !prev);
   };
-  <EditProfile showEditProfile={showEditProfile} setEditProfile={setEditProfile} />
-  <button className="EditProfile" onClick={openEditProfile}><LockIcon/></button>
-  */
+  
   return (
-    <div className="profileHead">
-      <img className="logo" src={Logo} alt="Logo"></img>
-      <div className="profile">
-        <div>
-          <img
-            className="profileImage"
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="profile"
-          />
+    <div>
+      <div className="profileHead">
+        <img className="logo" src={Logo} alt="Logo"></img>
+        <div className="profile">
+          <div>
+            <img
+              className="profileImage"
+              src="https://www.w3schools.com/howto/img_avatar.png"
+              alt="profile"
+            />
+            <button className="EditImage"><AttachmentIcon /></button>
 
-          <form className="SearchBarContainer">
-            <input className="Searchbar" placeholder="Zoeken" type="text"></input>
-            <button className="SearchButton" onClick={localStorage.setItem("Search",{Search})}><SearchIcon/></button>
-          </form>
+            <form className="SearchBarContainer">
+              <input className="Searchbar" placeholder="Zoeken" type="text"></input>
+              <button className="SearchButton" onClick={localStorage.setItem("Search",{Search})}><SearchIcon/></button>
+            </form>
 
+            
+            
+            <div className="profileContainer">
+              <h2>Profiel</h2>
+              <div className="ProfileDataNamesContainer">
+                <div className="ProfileDataNamesContainer">Naam</div>
+                <div className="ProfileDataNamesContainer">Email</div>
+                <div className="ProfileDataNamesContainer">Telefoonnummer</div>
+                <div className="ProfileDataNamesContainer">Email verzorger</div>
+                <div className="ProfileDataNamesContainer">Telefoonnummer verzorger</div>
+                <div className="ProfileDataNamesContainer">Straat</div>
+                <div className="ProfileDataNamesContainer">Huisnummer</div>
+                <div className="ProfileDataNamesContainer">Woonplaats</div>
+                <div className="ProfileDataNamesContainer">Postcode</div>
+              </div>
+              <div className="profileData">
+                <div className="dataBoxes">Hogeschool Rotterdam</div>
+                <div className="dataBoxes">hr@outlook.com</div>
+                <div className="dataBoxes">010 794 4000</div>
+                <div className="dataBoxes">0000000@hr.nl</div>
+                <div className="dataBoxes">010 794 4000</div>
+                <div className="dataBoxes">Wijnhaven</div>
+                <div className="dataBoxes">107</div>
+                <div className="dataBoxes">Rotterdam</div>
+                <div className="dataBoxes">3011 WN</div>
+              </div>
+            </div>
+
+            <EditProfile showEditProfile={showEditProfile} setEditProfile={setEditProfile} />
+            <button className="EditProfile" onClick={openEditProfile}><LockIcon/></button>
           
-          <button className="EditProfile"><LockIcon/></button>
-
-          <div className="profileContainer">
-            <h2>Profiel</h2>
-            <div className="ProfileDataNamesContainer">
-              <p>{localStorage.getItem({Search})}</p>
-              <p>Naam</p>
-              <p>Email</p>
-              <p>Telefoonnummer</p>
-              <p>Email verzorger</p>
-              <p>Telefoonnummer verzorger</p>
-              <p>Straat</p>
-              <p>Huisnummer</p>
-              <p>Woonplaats</p>
-              <p>Postcode</p>
-            </div>
-            <div className="profileData">
-              <div className="dataBoxes">Hogeschool Rotterdam</div>
-              <div className="dataBoxes">hr@outlook.com</div>
-              <div className="dataBoxes">010 794 4000</div>
-              <div className="dataBoxes">0000000@hr.nl</div>
-              <div className="dataBoxes">010 794 4000</div>
-              <div className="dataBoxes">Wijnhaven</div>
-              <div className="dataBoxes">107</div>
-              <div className="dataBoxes">Rotterdam</div>
-              <div className="dataBoxes">3011 WN</div>
-            </div>
           </div>
         </div>
       </div>
