a08a550d8d479dd878452b5fa89ae4e7e1e255f5
DzemalMRVC <70542783+DzemalMRVC@users.noreply.github.com>
2022-06-20 18:13:15 +0200

    Backend en Login
    jaaa

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,11 @@ import Navigation from "./Components/Navbar/navbar";
 import Feed from "./Components/Feed/feed";
 import Profile from "./Components/Profile/profile.js";
 
+import Login from "./Components/Login/login.js"
+
 const App = () => {
-  return <Navigation />;
+  // return <Navigation />;
+  return <Login />;
 };
 
 export default App;
