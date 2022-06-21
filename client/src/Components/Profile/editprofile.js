c3392c4e2a0a08a19b6596a938ac790956bad633
JoelvanSeventer <80532479+JoelvanSeventer@users.noreply.github.com>
2022-06-21 15:14:40 +0200

    editprofile fix

--- client/src/Components/Profile/editprofile.js
+++ client/src/Components/Profile/editprofile.js
@@ -8,6 +8,7 @@ import { useState, useEffect } from "react";
 
 export default function EditProfile() {
   const [Status, setStatus] = useState(1);
+  const [change, setChange] = useState();
 
   const [Naam, setNaam] = useState("");
   const [Email, setEmail] = useState("");
@@ -19,143 +20,105 @@ export default function EditProfile() {
   const [Woonplaats, setWoonplaats] = useState("");
   const [Postcode, setPostcode] = useState("");
 
-  const CheckForData = async () => {
-    try {
-      const Username = "piet";
-      //const Username = localStorage.getItem("username")
-      const request = await fetch(`http://localhost:8500/Server/users/${Username}`);
-      const data = await request.json();
-      if (data[0].Naam !== "") {
-        localStorage.setItem("Naam", data[0].Naam);
-        localStorage.setItem("Email", data[0].Email);
-        localStorage.setItem("Telefoonnummer", data[0].Telefoonnummer);
-        localStorage.setItem("Emailverzorger", data[0].Emailverzorger);
-        localStorage.setItem("Telefoonnummerverzorger", data[0].Telefoonnummerverzorger);
-        localStorage.setItem("Straat", data[0].Straat);
-        localStorage.setItem("Huisnummer", data[0].Huisnummer);
-        localStorage.setItem("Woonplaats", data[0].Woonplaats);
-        localStorage.setItem("Postcode", data[0].Postcode);
-      } else {
-        setStatus(2);
-        
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
+  const loadValues = async () => {
+    setNaam(localStorage.getItem('Naam'));
+    setEmail(localStorage.getItem('Email'));
+    setTelefoonnummer(localStorage.getItem('Telefoonnummer'));
+    setEmailverzorger(localStorage.getItem('Emailverzorger'));
+    setTelefoonnummerverzorger(localStorage.getItem('Telefoonnummerverzorger'));
+    setStraat(localStorage.getItem('Straat'));
+    setHuisnummer(localStorage.getItem('Huisnummer'));
+    setWoonplaats(localStorage.getItem('Woonplaats'));
+    setPostcode(localStorage.getItem('Postcode'));
   }
 
-  // useEffect(() => {
-  //   CheckForData()
-  // }, [])
+  const PreventSubmit = (event) => {
+    if(change){
+      event.preventDefault();
+      console.log("submission prevented");
+    }
+    setStatus(0);
+  };
+
+  useEffect(() => {
+    loadValues()
+  }, [])
 
   if (Status === 0) {
     return <ProfileInfo />;
   }
-  else if(Status === 2) {
-    return(
-      <div>
-        <h1>Error! Profile not found!</h1>
-        <ProfileInfo />
-      </div>
-    );
-  }
-  
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    localStorage.setItem("Naam", JSON.stringify(Naam));
-    localStorage.setItem("Email", JSON.stringify(Email));
-    localStorage.setItem("Telefoonnummer", JSON.stringify(Telefoonnummer));
-    localStorage.setItem("Emailverzorger", JSON.stringify(Emailverzorger));
-    localStorage.setItem(
-      "Telefoonnummerverzorger",
-      JSON.stringify(Telefoonnummerverzorger)
-    );
-    localStorage.setItem("Straat", JSON.stringify(Straat));
-    localStorage.setItem("Huisnummer", JSON.stringify(Huisnummer));
-    localStorage.setItem("Woonplaats", JSON.stringify(Woonplaats));
-    localStorage.setItem("Postcode", JSON.stringify(Postcode));
-  };
 
   return (
     <div>
       <div className="profileHead">
         <div>
           <div>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={PreventSubmit}>
               <div className="EditprofileData">
                 <input
                   className="EditdataBoxes"
                   title="Naam"
-                  value={Naam}
-                  placeholder={localStorage.getItem("Naam")}
-                  onChange={(e) => setNaam(e.target.value)}
+                  placeholder={Naam}
+                  onChange={(e) =>  localStorage.setItem('Naam', e.target.value)}
                 ></input>
                 <input
                   className="EditdataBoxes"
                   title="Email"
-                  value={Email}
-                  placeholder={localStorage.getItem("Email")}
-                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder={Email}
+                  onChange={(e) => localStorage.setItem('Email', e.target.value)}
                 ></input>
                 <input
                   className="EditdataBoxes"
                   title="Telefoonnummer"
-                  value={Telefoonnummer}
-                  placeholder={localStorage.getItem("Telefoonnummer")}
-                  onChange={(e) => setTelefoonnummer(e.target.value)}
+                  placeholder={Telefoonnummer}
+                  onChange={(e) => localStorage.setItem('Telefoonnummer', e.target.value)}
                 ></input>
                 <input
                   className="EditdataBoxes"
                   title="Email verzorger"
-                  value={Emailverzorger}
-                  placeholder={localStorage.getItem("Email verzorger")}
-                  onChange={(e) => setEmailverzorger(e.target.value)}
+                  placeholder={Emailverzorger}
+                  onChange={(e) => localStorage.setItem('Emailverzorger', e.target.value)}
                 ></input>
                 <input
                   className="EditdataBoxes"
                   title="Telefoonnummer verzorger"
                   id="Telefoonnummerverzorger"
-                  value={Telefoonnummerverzorger}
-                  placeholder={localStorage.getItem("Telefoonnummer verzorger")}
-                  onChange={(e) => setTelefoonnummerverzorger(e.target.value)}
+                  placeholder={Telefoonnummerverzorger}
+                  onChange={(e) => localStorage.setItem('Telefoonnummerverzorger', e.target.value)}
                 ></input>
                 <input
                   className="EditdataBoxes"
                   title="Straat"
                   id="Straat"
-                  value={Straat}
-                  placeholder={localStorage.getItem("Straat")}
-                  onChange={(e) => setStraat(e.target.value)}
+                  placeholder={Straat}
+                  onChange={(e) => localStorage.setItem('Straat', e.target.value)}
                 ></input>
                 <input
                   className="EditdataBoxes"
                   title="Huisnummer"
                   id="Huisnummer"
-                  value={Huisnummer}
-                  placeholder={localStorage.getItem("Huisnummer")}
-                  onChange={(e) => setHuisnummer(e.target.value)}
+                  placeholder={Huisnummer}
+                  onChange={(e) => localStorage.setItem('Huisnummer', e.target.value)}
                 ></input>
                 <input
                   className="EditdataBoxes"
                   title="Woonplaats"
                   id="Woonplaats"
-                  value={Woonplaats}
-                  placeholder={localStorage.getItem("Woonplaats")}
-                  onChange={(e) => setWoonplaats(e.target.value)}
+                  placeholder={Woonplaats}
+                  onChange={(e) => localStorage.setItem('Woonplaats', e.target.value)}
                 ></input>
                 <input
                   className="EditdataBoxes"
                   title="Postcode"
                   id="Postcode"
-                  value={Postcode}
-                  placeholder={localStorage.getItem("Postcode")}
-                  onChange={(e) => setPostcode(e.target.value)}
+                  placeholder={Postcode}
+                  onChange={(e) => localStorage.setItem("Postcode", e.target.value)}
                 ></input>
               </div>
-              <button className="Backbutton" title="Annuleren" onClick={() => setStatus(0)}>
+              <button className="Backbutton" title="Annuleren" onClick={() => setChange(false)}>
                 <CancelIcon />
               </button>
-              <button className="Savebutton" onClick={() => setStatus(0)}>
+              <button className="Savebutton" onClick={() => setChange(true)}>
                 Opslaan
               </button>
             </form>
--- client/src/Components/Profile/profileinfo.js
+++ client/src/Components/Profile/profileinfo.js
@@ -7,43 +7,45 @@ import EditProfile from "./editprofile.js";
 export default function ProfileInfo() {
   const [Status, setStatus] = useState(0);
 
-  const [Naam, setNaam] = useState("");
-  const [Email, setEmail] = useState("");
-  const [Telefoonnummer, setTelefoonnummer] = useState("");
-  const [Emailverzorger, setEmailverzorger] = useState("");
-  const [Telefoonnummerverzorger, setTelefoonnummerverzorger] = useState("");
-  const [Straat, setStraat] = useState("");
-  const [Huisnummer, setHuisnummer] = useState("");
-  const [Woonplaats, setWoonplaats] = useState("");
-  const [Postcode, setPostcode] = useState("");
+  const [Naam, setNaam] = useState();
+  const [Email, setEmail] = useState();
+  const [Telefoonnummer, setTelefoonnummer] = useState();
+  const [Emailverzorger, setEmailverzorger] = useState();
+  const [Telefoonnummerverzorger, setTelefoonnummerverzorger] = useState();
+  const [Straat, setStraat] = useState();
+  const [Huisnummer, setHuisnummer] = useState();
+  const [Woonplaats, setWoonplaats] = useState();
+  const [Postcode, setPostcode] = useState();
 
   const loadValues = async () => {
-    setNaam(localStorage.getItem("Naam"));
-    setEmail(localStorage.getItem("Email"));
-    setTelefoonnummer(localStorage.getItem("Telefoonnummer"));
-    setEmailverzorger(localStorage.getItem("Emailverzorger"));
-    setTelefoonnummerverzorger(localStorage.getItem("Telefoonnummerverzorger"));
-    setStraat(localStorage.getItem("Straat"));
-    setHuisnummer(localStorage.getItem("Huisnummer"));
-    setWoonplaats(localStorage.getItem("Woonplaats"));
-    setPostcode(localStorage.getItem("Postcode"));
+    setNaam(localStorage.getItem('Naam'));
+    setEmail(localStorage.getItem('Email'));
+    setTelefoonnummer(localStorage.getItem('Telefoonnummer'));
+    setEmailverzorger(localStorage.getItem('Emailverzorger'));
+    setTelefoonnummerverzorger(localStorage.getItem('Telefoonnummerverzorger'));
+    setStraat(localStorage.getItem('Straat'));
+    setHuisnummer(localStorage.getItem('Huisnummer'));
+    setWoonplaats(localStorage.getItem('Woonplaats'));
+    setPostcode(localStorage.getItem('Postcode'));
   }
 
   const setValues = async () => {
-    localStorage.setItem("Username", "Testuser");
-    localStorage.setItem("Naam", "Naam");
-    localStorage.setItem("Email", "Email");
-    localStorage.setItem("Telefoonnummer", "Telefoonnummer");
-    localStorage.setItem("Emailverzorger", "Email verzorger");
-    localStorage.setItem("Telefoonnummerverzorger", "Telefoonnummer verzorger");
-    localStorage.setItem("Straat", "Straat");
-    localStorage.setItem("Huisnummer", "Huisnummer");
-    localStorage.setItem("Woonplaats", "Woonplaats");
-    localStorage.setItem("Postcode", "Postcode");
+    localStorage.setItem('Username', 'Testuser');
+    localStorage.setItem('Naam', 'Hogeschool Rotterdam');
+    localStorage.setItem('Email', '10000000@hr.nl');
+    localStorage.setItem('Telefoonnummer', '010-12345678');
+    localStorage.setItem('Emailverzorger', 'asdf@gmail.com');
+    localStorage.setItem('Telefoonnummerverzorger', '010-12345678');
+    localStorage.setItem('Straat', 'Teststraat');
+    localStorage.setItem('Huisnummer', '1');
+    localStorage.setItem('Woonplaats', 'Rotterdam');
+    localStorage.setItem('Postcode', '3035');
   }
 
   useEffect(() => {
-    setValues()
+    if(localStorage.getItem('Username') === null) {
+      setValues()
+    }
     loadValues()
   }, [])
 
@@ -78,16 +80,3 @@ export default function ProfileInfo() {
     );
   }
 }
-
-/*
-                  <div className="dataBoxes" title="Naam">Hogeschool Rotterdam</div>
-                  <div className="dataBoxes" title="Email">hr@outlook.com</div>
-                  <div className="dataBoxes" title="Telefoonnummer">010 794 4000</div>
-                  <div className="dataBoxes" title="Email verzorger">0000000@hr.nl</div>
-                  <div className="dataBoxes" title="Telefoonnummer verzorger">010 794 4000</div>
-                  <div className="dataBoxes" title="Straat">Wijnhaven</div>
-                  <div className="dataBoxes" title="Huisnummer">107</div>
-                  <div className="dataBoxes" title="Woonplaats">Rotterdam</div>
-                  <div className="dataBoxes" title="Postcode">3011 WN</div>
-
-*/
