50b124a22d1c406ee5bfbfb33795ced8d52f52ac
DanIsVis <danielvisser.nl@gmail.com>
2022-06-22 01:55:30 +0200

    44t4
    3rr

--- Server/db.js
+++ Server/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "qwerty",
+  password: "daan7272!",
   host: "localhost",
   port: "5432",
   database: "backend"
